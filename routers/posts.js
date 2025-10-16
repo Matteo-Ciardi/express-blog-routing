8dd9caee53ee7c0b8bdcc6d8da7e874c78143d21
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-16 15:52:01 +0200

    creato server.js e posts.js

---
+++ routers/posts.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send('Lista dei post');
+})
+
+router.get('/:id', function (req, res) {
+    res.send('Dettagli dei post' + req.params.id);
+})
+
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
+})
+
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale del post' + req.params.id);
+})
+
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale del post' + req.params.id);
+})
+
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione post' + req.params.id);
+})
+
+module.esports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const postsRouter = require('./routers/posts')
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    console.log('Hai richiesto index')
+
+    res.send('<h1>Home blog</h1>')
+})
+
+app.use('/pizzas', postsRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listenint on port ${port}`)
+});
